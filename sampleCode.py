1d414bd5d35bd605a7a97988b0ca859336f9cfa4
alanpadillachua <axp141330@utdallas.edu>
2017-04-22 03:34:33 -0500

    test file complete

--- sampleCode.py
+++ sampleCode.py
@@ -1,4 +1,23 @@
-import _mysql_connector
-print("hello world")
-cnx = _mysql_connector.connect(user='root', database ='grrs')
-cursor = cnx.cursor()
+import mysql.connector
+
+
+print("Death Troopers")
+
+config = {
+    'user': 'root',
+    'password': '214334dJ!',
+    'host': '127.0.0.1',
+    'database': 'grrs',
+    'raise_on_warnings': True,
+}
+
+connector = mysql.connector.connect(**config)
+
+cursor = connector.cursor()
+
+cursor.execute("SELECT * FROM jedi_general")
+
+for row in cursor.fetchall():
+    print(row[0])
+
+connector.close()
