86abd5633c444e9f24fb015477e460696b354683
alanpadillachua <axp141330@utdallas.edu>
2017-04-24 21:27:45 -0500

    added more methods that populate table

--- DeathTroopers.py
+++ DeathTroopers.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import random
-
+import re
 
 # print("Death Troopers")
 
@@ -16,7 +16,7 @@ connector = mysql.connector.connect(**config)
 cursor = connector.cursor()
 
 
-def insert(query, data):
+def do_query(query, data):
     try:
         cursor.execute(query, data)
         connector.commit()
@@ -29,7 +29,7 @@ def insert_station():
                    '(STATION_ID, x_axis, y_axis, z_axis)'
                    'VALUES (%s,%s,%s,%s)')
     data_station = ('1', '10023.2', '29001.9', '120.21')
-    insert(add_station, data_station)
+    do_query(add_station, data_station)
 
 
 def insert_rooms():
@@ -40,7 +40,7 @@ def insert_rooms():
     while num <= 50:
         # print(num)
         data_rooms = ('1', '%d' % num)
-        insert(add_rooms, data_rooms)
+        do_query(add_rooms, data_rooms)
         num += 1
 
 
@@ -52,7 +52,7 @@ def insert_training_room():
 
     while num <= 25:
         data_training_room = ('1', '%d' % num, '25', '1')
-        insert(add_training_room, data_training_room)
+        do_query(add_training_room, data_training_room)
         num += 1
 
 
@@ -63,7 +63,7 @@ def insert_briefing_room():
                          'VALUES (%s,%s,%s,%s)')
     while num <= 50:
         data_briefing_room = ('1', '%d' % num, '25', '1')
-        insert(add_briefing_room,data_briefing_room)
+        do_query(add_briefing_room, data_briefing_room)
         num += 1
 
 
@@ -72,7 +72,7 @@ def insert_landing_station():
                            '(Lstation_ID,GRRS_ID,Amount_of_FDocks,Amount_of_Docks,Amount_of_IUDocks)'
                            'VALUES (%s,%s,%s,%s,%s)')
     data_landing_station = ('1313', '1', '20', '40', '20')
-    insert(add_landing_station,data_landing_station)
+    do_query(add_landing_station, data_landing_station)
 
 
 def insert_landing_dock():
@@ -82,7 +82,7 @@ def insert_landing_dock():
                         'VALUES (%s,%s,%s,%s)')
     while num <= 40:
         data_landing_dock = ('1313', '%d' % num, '1', '23')
-        insert(add_landing_dock, data_landing_dock)
+        do_query(add_landing_dock, data_landing_dock)
         num += 1
 
 
@@ -105,7 +105,7 @@ def insert_mission():
         # print('index ' + str(i) + ' ' + mission_type_clean_list[i] + ' | ' + mission_goal_clean_list[i])
         data_mission = ('%s' % str(mission_num + i), '%s' % mission_goal_clean_list[i],
                         '20', '20', '%s' % mission_type_clean_list[i], '1000')
-        insert(add_mission, data_mission)
+        do_query(add_mission, data_mission)
 
 
 def insert_jedi():
@@ -136,8 +136,73 @@ def insert_jedi():
                      '%s' % str(mission_num + i),  # assign it mission in order
                      '%s' % str(random.randrange(100, 5000)),  # random credit amount
                      '2223-04-24 12:12:00')
-        insert(add_jedi, data_jedi)
+        do_query(add_jedi, data_jedi)
 
+
+def insert_spaceship():
+    add_spaceship = ('INSERT INTO spaceship'
+                     '(JCall_Sign,SS_ID)'
+                     'VALUES (%s,%s)')
+    get_jedi_call_signs = 'SELECT jcall_sign FROM jedi_general'
+    cursor.execute(get_jedi_call_signs)
+    call_sign_list = list(cursor.fetchall())
+    num = 1770
+    for line in call_sign_list:
+        data_spaceship = ('%s' % line[0],
+                          '%d' % num)
+        do_query(add_spaceship, data_spaceship)
+        num += 1
+
+
+def pop_must_dock():
+    get_ss_id = 'SELECT SS_ID FROM spaceship'
+    cursor.execute(get_ss_id)
+    ssid_list = list(cursor.fetchall())
+
+    ssid_length = len(ssid_list)
+    get_empty_docks = 'SELECT Dock_Number FROM landing_dock WHERE isAvailable = 1 LIMIT ' + str(ssid_length)
+    cursor.execute(get_empty_docks)
+    dock_number_list = list(cursor.fetchall())
+    add_must_dock = ('INSERT INTO must_dock'
+                     '(SS_ID, Dock_Number) VALUES (%s, %s)')
+    for i in range(0, ssid_length):
+        data_must_dock = ('%s' % ssid_list[i],
+                          '%s' % dock_number_list[i])
+        # print(data_must_dock)
+        do_query(add_must_dock, data_must_dock)
+
+    # update landing_dock isAvailable to 0
+    get_dock_num = 'SELECT Dock_Number FROM must_dock'
+    cursor.execute(get_dock_num)
+    dock_list = list(cursor.fetchall())
+    length = len(dock_list)
+    for i in range(0, length):
+        update_landing_dock = 'UPDATE landing_dock SET isAvailable = 0 WHERE Dock_Number = ' + '%s' % dock_list[i]
+        cursor.execute(update_landing_dock)
+        connector.commit()  # commit changes
+
+
+def pop_must_pay_fee():
+    get_jedi_call_signs = 'SELECT jcall_sign FROM spaceship'
+    cursor.execute(get_jedi_call_signs)
+    call_sign_list = list(cursor.fetchall())
+
+    get_station_id = 'SELECT LStation_ID FROM landing_dock WHERE isAvailable = 0 '
+    cursor.execute(get_station_id)
+    station_id_list = list(cursor.fetchall())
+
+    get_price = 'SELECT Rental_price from landing_dock WHERE isAvailable = 0'
+    cursor.execute(get_price)
+    price_list = list(cursor.fetchall())
+
+    add_must_pay_fee = ('INSERT INTO must_pay_fee'
+                        '(JGCS_ID, LSID, Fee_Amt)'
+                        ' VALUES (%s,%s,%s)')
+    for i in range(0, len(call_sign_list)):
+        must_pay_data = ('%s' % call_sign_list[i],
+                         '%s' % station_id_list[i],
+                         '%s' % price_list[i])
+        do_query(add_must_pay_fee, must_pay_data)
 
 
 
@@ -149,8 +214,10 @@ def insert_jedi():
 # insert_landing_dock()
 # insert_mission()
 # insert_jedi()
-
-cursor.execute("SELECT * FROM jedi_general")
+# insert_spaceship()
+# pop_must_dock()
+# pop_must_pay_fee()
+cursor.execute("SELECT * FROM must_pay_fee")
 for row in cursor.fetchall():
     print(row)
 
